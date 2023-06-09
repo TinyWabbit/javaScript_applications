c479e0215953a52d22f8e031c304ee72ae2ca31d
TinyWabbit <resonance369@protonmail.com>
2023-06-09 11:30:13 +0100

    Add employeeTracker app to repo

---
+++ employeeTracker.js
@@ -0,0 +1,17 @@
+class Employees {
+    constructor (firstname, lastname, yearsnumber) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.yearsnumber = yearsnumber;
+    }
+}
+const Employee1 = new Employees("George", "Mason", 7);
+const Employee2 = new Employees("Humphrey", "Taylor", 3);
+const employeeArray = [Employee1, Employee2];
+
+Employees.prototype.returnCredentials = function() {
+    return this.firstname + " " + this.lastname + " has worked here for " + this.yearsnumber + " years ";
+}
+employeeArray.forEach((Employee) => {
+   console.log(Employee.returnCredentials());
+});
